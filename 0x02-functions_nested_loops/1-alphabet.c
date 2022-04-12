40738d2732ffd99b620dbf588815d4948bc9c74e
kingsleychigozirim <ruelnicyp@gmail.com>
2022-04-12 08:49:17 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
+
 /**
-*print_alphabet - function that print alphabet
-*it prints in lowercase
-*Return: no return
-*/
-void print_alphabet(void)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-int letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar (letter);
-}
-_putchar (10);
+    print_alphabet();
+    return (0);
 }
