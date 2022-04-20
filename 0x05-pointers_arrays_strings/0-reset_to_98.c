658cfe87e24d06357b6584faa974b507179ccfc4
kingsleychigozirim <ruelnicyp@gmail.com>
2022-04-20 13:55:24 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,1 @@
-#include "main.h"
 
-/**
- *reset_to_98 - update a integer value to 98
- *on: A pointer to an int that will be updated
- *
- *Return: void that means is correct
- */
-
-void reset_to_98(int *n)
-{
-
-*n = 98;
-
-}
